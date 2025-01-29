ec62e987e5106a4c9a5299246a966c3e3f00f10c
Gaks3 <dwi66116@gmail.com>
2025-01-29 15:53:07 +0700

    feat: create show product page by slug

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Resources\ProductListResource;
+use App\Http\Resources\ProductResource;
 use App\Models\Product;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
@@ -18,8 +19,11 @@ class ProductController extends Controller
         ]);
     }
 
-    public function show()
+    public function show(Product $product)
     {
-        return dd('hello');
+        return Inertia::render('Product/Show', [
+            'product' => new ProductResource($product),
+            'variationOptions' => request('options', [])
+        ]);
     }
 }
